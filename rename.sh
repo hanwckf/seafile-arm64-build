38062e11afcd1f93ece40b7594efa4d6f587e53c
hanwckf <hanwckf@vip.qq.com>
2020-09-01 00:16:49 +0800

    add alpine-based build

---
+++ rename.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+[ -z "$1" ] && exit 1
+
+cd seafile-build/output || exit 1
+
+for x in `ls`; do
+	new_filename=`sed "s#.tar.gz#-$1.tar.gz#" <<< $x`
+	mv $x $new_filename
+done
+
---
+++ seafile-build/alpine-prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "$TRAVIS" ]; then
+	sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories
+fi
+
+apk update
+
+apk add --no-progress \
+	gcc g++ make git cmake sudo intltool sqlite-dev py3-pip \
+	libarchive-dev curl-dev glib libtool vala libxml2 \
+	libevent-dev oniguruma-dev fuse-dev jansson-dev mariadb-dev \
+	openssl-dev libxslt-dev util-linux-dev py3-setuptools \
+	py3-lxml pkgconfig patch autoconf automake gzip \
+	openldap-dev bsd-compat-headers
+
